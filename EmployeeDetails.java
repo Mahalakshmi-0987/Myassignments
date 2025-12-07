05ccf912d99af160b75250003ea603671bd0b01b
Mahalakshmi-0987 <thiruan@gmail.com>
2025-12-07 13:16:41 +0530

    Add files via upload

---
+++ EmployeeDetails.java
@@ -0,0 +1,19 @@
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		String name="John Doe";
+		int age=30;
+		float salary=55000.75f;
+		char gender='M';
+		boolean acceptTerms=true;
+		float rating=4.5f;
+		long mobilenumber=9876543210L;
+		int pincode=560012;
+		byte luckynumber=7;
+		System.out.println("Employee Name:"+name+"\nAge :"+age+"\nSalary :"+salary+"\nGender :"+gender+"\nAccept Terms:"+acceptTerms+"\nRating :"+rating+"\nMobile NO :"+mobilenumber+"\nPincode :"+pincode+"\nLucky Number :"+luckynumber);
+	}
+
+}
